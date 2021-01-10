5313f8f74d2c58874b54fcc7104cd608e28de7bf
zenyuan <zenyuan960122@163.com>
2021-01-10 18:30:18 +0800

    get normal of intersection which sphere and ray

--- include/ray.h
+++ include/ray.h
@@ -2,6 +2,7 @@
 #define RAY_H
 #include "vec.h"
 #include "color.h"
+#include <cmath>
 
 namespace pzyy {
 class ray {
@@ -10,7 +11,7 @@ class ray {
     ray(const point& origin, const vec3f& director):_origin(origin), _director(director) {}
     point orign() const { return _origin; }
     vec3f director() const { return _director; }
-    vec3f at(double t) { return _origin + t * _director; }
+    vec3f at(double t) const { return _origin + t * _director; }
  private:
     point _origin;
     vec3f _director;
@@ -21,21 +22,27 @@ class ray {
 2.sphere center 圆心
 3.radius 半径
 */
-bool hitSphere(const ray& r, const point& SphereCenter, double radius) {
+double hitSphere(const ray& r, const point& SphereCenter, double radius) {
     //origin - center 
     vec3f co = r.orign() - SphereCenter;
     //squre root b^2-4ac
     double a = dot(r.director(), r.director());
     double b = 2.0*dot(r.director(), co);
     double c = dot(co, co)-radius*radius;
-    bool intersection = (b*b-4*a*c >= 0)?1:0;
-    return intersection;
+    double intersection = b*b-4*a*c;
+    if(intersection < 0.0f) {
+        return -1.0;
+    }
+    return (-b - std::sqrt(intersection))/(2.0*a);
 }
 
-color ray_color(const ray& r) {    
-    /*define sphere equation*/
-    if(hitSphere(r, point(0, 0, -1), 0.5)) {
-        return color(1.0, 0.0, 0.0);
+color ray_color(const ray& r) {
+    /*ray param of t*/  
+    auto t = hitSphere(r, point(0, 0, -1), 0.5);
+    if(t > 0.0) {
+        //sphereNormal every component [-1.0, 1.0]
+        vec3f sphereNormal = identity(r.at(t) - point(0, 0, -1));
+        return 0.5*color(sphereNormal.x+1, sphereNormal.y+1, sphereNormal.z+1);
     }
     /*calculate backfround color*/
     //identity of ray director(-1.0 < y < 1.0)
